e52ca0fe855b3f93aae951c055ee0a62b4f01a48
Gheo56 <h.liubovich@gmail.com>
2025-10-31 19:06:02 +0100

    Закрытые по Enter

--- HelloWorld.go
+++ HelloWorld.go
@@ -6,46 +6,51 @@ import (
 )
 
 func main() {
-	PrintHeader("Калькулятор Жирности Массы Тела 💪")
-	userKg, userHeight := getUserInput()
-	IMT := calculateIMT(userKg, userHeight)
+	PrintHeader1("Калькулятор Жирности Массы Тела 💪")
+	userKg1, userHeight1 := getUserInput1()
+	IMT1 := calculateIMT1(userKg1, userHeight1)
 
-	clearScreen()
+	clearScreen1()
 
-	outputResult(IMT)
-	fmt.Println("\nЧто бы выйти введите любую клавишу и нажмите Enter")
-	var a string
-	fmt.Scan(&a)
+	outputResult1(IMT1)
+
+	GetUserExit1()
 }
 
-func outputResult(IMT float64) {
-	result := fmt.Sprintf("Ваша жирность массы тела: %.0f", IMT)
-	fmt.Print(result)
+func outputResult1(IMT1 float64) {
+	result1 := fmt.Sprintf("Ваша жирность массы тела: %.0f", IMT1)
+	fmt.Print(result1)
 }
 
-func calculateIMT(userKg float64, userHeight float64) float64 {
+func calculateIMT1(userKg1 float64, userHeight1 float64) float64 {
 	const IMTPower = 2
 	const Meter = 100
-	IMT := userKg / math.Pow(userHeight/Meter, IMTPower)
-	return IMT
+	IMT1 := userKg1 / math.Pow(userHeight1/Meter, IMTPower)
+	return IMT1
 }
-func getUserInput() (float64, float64) {
-	var userHeight float64
-	var userKg float64
+func getUserInput1() (float64, float64) {
+	var userHeight1 float64
+	var userKg1 float64
 	fmt.Print("Введите свой рост (в сантиметрах): ")
-	fmt.Scan(&userHeight)
+	fmt.Scanln(&userHeight1)
 	fmt.Print("Введите свой вес (в килограммах): ")
-	fmt.Scan(&userKg)
-	clearScreen()
-	return userKg, userHeight
+	fmt.Scanln(&userKg1)
+	return userKg1, userHeight1
+
 }
-func PrintHeader(title string) {
+func PrintHeader1(title string) {
 	fmt.Println("\033[36m╔════════════════════════════════════════════╗")
 	fmt.Printf("║ %-32s         ║\n", title)
 	fmt.Println("╚════════════════════════════════════════════╝\033[0m")
 	fmt.Println()
 }
 
-func clearScreen() {
+func clearScreen1() {
 	fmt.Print("\033[H\033[2J")
+}
+func GetUserExit1() string {
+	var ab1 string
+	fmt.Println("\nНажмите Enter для выхода...")
+	fmt.Scanln(&ab1)
+	return ab1
 }
